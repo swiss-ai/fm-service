c5125b0247ab0d3cbd446eae414541a61565c336
Xiaozhe Yao <askxzyao@gmail.com>
2025-12-22 18:37:12 +0100

    minor

--- apps/home/.astro/content.d.ts
+++ apps/home/.astro/content.d.ts
@@ -193,6 +193,14 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"images": Record<string, {
+  id: string;
+  body?: string;
+  collection: "images";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
 
 	};
 
